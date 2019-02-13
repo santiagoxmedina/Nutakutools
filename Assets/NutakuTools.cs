38ac2665f5d20e4cf88a43d31fe0b70e483e65ba
Santiago Medina <santiago@timba.co>
2019-02-13 15:32:03 -0500

    first commit

---
+++ Assets/NutakuTools.cs
@@ -0,0 +1,90 @@
+﻿using AOT;
+using System;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class NutakuTools : MonoBehaviour {
+
+    [DllImport("__Internal")]
+    private static extern void CheckGuestPlayer (Action<string> action);
+    [DllImport("__Internal")]
+    private static extern void OpenGuestPlayerFrom ();
+    [DllImport("__Internal")]
+    private static extern void OpenCrossPromotion ();
+
+    private static Action<string> m_action;
+    private static Action<string> m_actionError;
+
+    /// <summary>
+    /// Check the player nutaku grade
+    /// </summary>
+    /// <param name="action"> return the grade value</param>
+    /// <param name="actionError">return error message</param>
+    public void OnCheckGuestPlayer (Action<string> action, Action<string> actionError)
+    {
+        m_action += action;
+        m_actionError += actionError;
+        CheckGuestPlayer(Callback);
+    }
+
+    /// <summary>
+    /// Open Guest form
+    /// </summary>
+    public void OnOpenGuestPlayerFrom ()
+    {
+
+        OpenGuestPlayerFrom();
+    }
+    /// <summary>
+    /// Open Cross promotion banner
+    /// </summary>
+    public void OnOpenCrossPromotion()
+    {
+        OpenCrossPromotion();
+    }
+
+    #region test
+    public void OnCheckGuestPlayerTest ()
+    {
+        OnCheckGuestPlayer(OnTestResult,OnTestResultError);
+    }
+
+    private void OnTestResult (string result)
+    {
+        Debug.Log("OnTestResult  " + result);
+    }
+
+    private void OnTestResultError (string result)
+    {
+        Debug.Log("OnTestResultError: " + result);
+    }
+    #endregion
+
+    [MonoPInvokeCallback(typeof(Action<string>))]
+    private static void Callback (string arg)
+    {
+       
+        NutakuToolResponse nutakuToolResponse = JsonUtility.FromJson<NutakuToolResponse>(arg);
+        if(nutakuToolResponse.code == 0)
+        {
+            if (m_action != null)
+            {
+                m_action(nutakuToolResponse.result);
+            }
+        }
+        else
+        {
+            if (m_actionError != null)
+            {
+                m_actionError(nutakuToolResponse.result);
+            }
+        }
+        m_actionError = null;
+        m_action =null;
+    }
+}
+[System.Serializable]
+public class NutakuToolResponse {
+    public int code;
+    public string result;
+}
---
+++ Assets/Plugins/NutakuTools.jslib
@@ -0,0 +1,59 @@
+
+var LibraryJsCallCsTest = {
+	  	$JsCallCsTest: {},
+	  	CheckGuestPlayer: function (action) {
+	    	var params = {};
+			params[opensocial.DataRequest.PeopleRequestFields.PROFILE_DETAILS] = [nutaku.Person.Field.GRADE];
+			var req = opensocial.newDataRequest();
+			req.add(req.newFetchPersonRequest(opensocial.IdSpec.PersonId.VIEWER, params), "viewer");
+			req.send(function(response) {
+				var data = {};
+			    if (response.hadError()) {
+			        console.log("CheckGuestPlayer Error:"+JSON.stringify(response));
+			        data.code = -1;
+			    } else {
+			        var item = response.get("viewer");
+			        if (item.hadError()) {
+			            console.log("CheckGuestPlayer Error:"+JSON.stringify(response));
+			            data.code = -1;
+			        } else {
+			            var viewer = item.getData();
+			            data.code = 0;
+			            data.result = viewer.getField(nutaku.Person.Field.GRADE);
+			        }
+			    }
+			    var json = JSON.stringify(data);
+			    var bufferSize = lengthBytesUTF8(json) + 1;
+				var buffer = _malloc(bufferSize);
+				stringToUTF8(json, buffer, bufferSize);
+				JsCallCsTest.callback = action;
+				Runtime.dynCall('vi', JsCallCsTest.callback, [buffer]);
+				});
+	  	},
+
+	  	OpenGuestPlayerFrom: function () {
+	    	var params = {};
+			params[nutaku.GuestRequestFields.VERSION] = 1;
+
+		    var newGuestSignUpResult = opensocial.newGuestSignUp(params);
+		    opensocial.requestGuestSignUp(newGuestSignUpResult, function(response) {
+			    // Callback processing is performed once the user action on the signup modal is completed.   
+			    // However, if the user selects "Sign Up", the user will be directed to the signup page, thus the callback processing is not executed.
+			    if (!response.hadError()) {
+			        var responseCode = newGuestSignUpResult.getField(nutaku.Guest.Field.RESPONSE_CODE);
+			        console.log("OpenGuestPlayerFrom: "+responseCode);
+			    }
+			    else {
+			    //error handling
+			    console.log("OpenGuestPlayerFrom Error:"+JSON.stringify(response));
+			    }
+			});
+			    
+	  	},
+	  	OpenCrossPromotion: function () {
+	    	crossPromo.showBannerModal(console.log);
+	  	},
+
+};
+autoAddDeps(LibraryJsCallCsTest, '$JsCallCsTest');
+mergeInto(LibraryManager.library, LibraryJsCallCsTest);
